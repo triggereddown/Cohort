96ec65ccd6966af019f22db8bb6b9ec269609749
triggereddown <deepmoitra2@gmail.com>
2025-12-24 20:42:24 +0530

    Learning to import Cards using probs  object

--- ReactCard/src/App.jsx
+++ ReactCard/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Card from "./components/Card/Card";
 
 const App = () => {
   return (
     <div className="text-red-500">
       App
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      {/* Rendering 2 components of same type  */}
+      <Card user="Trigger" age="50" />
+      <Card user="Puttan" age="40" />
     </div>
   );
 };
