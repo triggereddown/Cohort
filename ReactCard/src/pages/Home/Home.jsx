67e389c11a4836b1aff2b618d5e198353a265e3d
triggereddown <deepmoitra2@gmail.com>
2026-01-13 00:59:30 +0530

    Updated button

--- ReactCard/src/pages/Home/Home.jsx
+++ ReactCard/src/pages/Home/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <div>
+        <h1 className="text-4xl font-bold py-6">
+          Welcome to Triggu's Home Page
+        </h1>
+        <div>
+          <button
+            onClick={() => {
+              navigate("/products");
+            }}
+            className="bg-emerald-600 p-2 rounded-md"
+          >
+            Explore Saas Section
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
